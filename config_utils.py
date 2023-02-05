4998e2ac7472e1ad2f73f346c323edf0e2af1c42
Chris Azer <azer@amazon.com>
2023-02-05 11:57:11 -0800

    Fixed exception logging

--- config_utils.py
+++ config_utils.py
@@ -24,8 +24,4 @@ EchoBotStatusGetPublish = ""
 
 # Get a logger
 logger = getLogger()
-handler = StreamHandler(stdout)
-formatter = logging.Formatter('[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s','%m-%d %H:%M:%S')
-handler.setFormatter(formatter)
 logger.setLevel(INFO)
-logger.addHandler(handler)
